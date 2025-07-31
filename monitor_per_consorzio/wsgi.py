0507f05384958cec7313dfd99e8bc0052c8cf630
stefanoZS <stefano.trevisan@zilioservice.com>
2025-07-31 14:31:14 +0200

    cambiato wsgi

--- monitor_per_consorzio/wsgi.py
+++ monitor_per_consorzio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'provaAzire3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitor_per_consorzio.settings')
 
 application = get_wsgi_application()
